b044bbce6d91c8a9a144ddc14d0073bf35ea5802
ER1-53 <cremey.erwan@gmail.com>
2024-01-04 18:46:33 +0100

    delete async before def

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """ measure time """
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
