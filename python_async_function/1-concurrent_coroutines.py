089c159d3acebc6faef0ca3dc394fe1a8a86c51e
ER1-53 <cremey.erwan@gmail.com>
2024-01-04 18:22:51 +0100

    modif position of import

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 import asyncio
 from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
 """ running win """
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """delay_n = await asyncio.gather(
-        *[wait_random(max_delay) for _ in range(n)])"""
+    """
+    delay_n = await asyncio.gather(
+        *[wait_random(max_delay) for _ in range(n)])
+    """
     list_of_delay = []
     for turn in range(n):
         list_of_delay.append(await wait_random(max_delay))
