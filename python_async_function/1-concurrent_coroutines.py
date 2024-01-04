5114a7558d9f04a32b63fabc38a7eebd1f9e8c2d
ER1-53 <cremey.erwan@gmail.com>
2024-01-04 18:26:50 +0100

    test with big comment

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,9 +7,22 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    delay_n = await asyncio.gather(
-        *[wait_random(max_delay) for _ in range(n)])
+    Perform asynchronous waiting using wait_random.
+
+    Args:
+        n (int): Number of times to call wait_random.
+        max_delay (int): Maximum delay value for each wait_random call.
+
+    Returns:
+        typing.List[float]: List of delays in ascending order.
+
+    Note:
+        This function uses asyncio.gather to concurrently run wait_random n
+        times.
+        The list of delays is returned in ascending order without using sort()
+        due to concurrency.
     """
+    
     list_of_delay = []
     for turn in range(n):
         list_of_delay.append(await wait_random(max_delay))
