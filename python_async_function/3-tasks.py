093a97b6bc2e101509e60b0d7124e159293043b1
ER1-53 <cremey.erwan@gmail.com>
2024-01-05 10:59:17 +0100

    task 3 p2 update methode

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,6 +6,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_random(max_delay: int) -> asyncio.Task:
-    """ task """
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ return a task """
     return asyncio.create_task(wait_random(max_delay))
