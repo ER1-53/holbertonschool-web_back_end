fba027c1fe05d6fb8bfac94dcc701f8f4ee05459
ER1-53 <cremey.erwan@gmail.com>
2024-01-05 11:12:07 +0100

    update i to _

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -12,7 +12,8 @@ from typing import Generator
 async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronously and random
+    loop IO
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
