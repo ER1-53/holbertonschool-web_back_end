2e591d2a8bdad6ba867ad71197ca16ce6024a2c7
ER1-53 <cremey.erwan@gmail.com>
2024-01-11 10:44:26 +0100

    task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     client = MongoClient('localhost', 27017)
     collection = client.logs.nginx
 
-    count = collection.count_documents({})
+    counts = collection.count_documents({})
 
-    print("{} logs".format(count))
-    
+    print("{} logs".format(counts))
+
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
@@ -20,5 +20,3 @@ if __name__ == "__main__":
     specific_query = {"method": "GET", "path": "/status"}
     specific_count = collection.count_documents(specific_query)
     print("{} status check".format(specific_count))
-
-
