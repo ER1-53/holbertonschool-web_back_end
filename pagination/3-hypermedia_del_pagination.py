ad40502a62ce78c3e5b7b0a407dc44363fe131df
ER1-53 <cremey.erwan@gmail.com>
2024-01-11 23:26:41 +0100

    add comment

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -38,6 +38,8 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """ return dictionary"""
+        
         assert 0 <= index < len(self.__indexed_dataset)
 
         next_index = index + page_size
