4e51aa71ee3ac4fe6ee1a584e4f31b23c1d63f94
ER1-53 <cremey.erwan@gmail.com>
2024-01-11 22:18:49 +0100

    modification

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,10 +3,14 @@
 function named index_range that takes
 two integer arguments page and page_size
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int):
-    """ index all page """
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    index all page
+    """
+    
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
+""" simple pagination"""
 import csv
 import math
 from typing import List
-""" simple pagination"""
 
 
-index_range = __import__('0-simple_helper_function').index_range
+def index_range(page: int, page_size: int):
+    """ index all page """
+    assert isinstance(page, int) and page > 0
+    assert isinstance(page_size, int) and page_size > 0
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
 
 
 class Server:
@@ -28,9 +34,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ return the good page of the dtaset """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
         start_index, end_index = index_range(page, page_size)
-        dataset = self.dataset()
-        return dataset[start_index:end_index]
+        return self.dataset()[start_index:end_index]
