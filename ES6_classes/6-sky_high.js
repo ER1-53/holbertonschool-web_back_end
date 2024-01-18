6acf6da07c1f193b3c470e2ba83b9f5d2b426e59
ER1-53 <cremey.erwan@gmail.com>
2024-01-18 17:26:12 +0100

    remove all new line

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,19 +1,15 @@
 import Building from './5-building';
-
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
-
   get floors() {
     return this._floors;
   }
-
   set floors(value) {
     this._floors = value;
   }
-
   evacutionWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
