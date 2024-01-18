a2c5655a246e43557f76fb925d45f447cdae6aed
ER1-53 <cremey.erwan@gmail.com>
2024-01-18 09:45:44 +0100

    change for dot

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -2,7 +2,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arrayNew = [];
   for (const index of array) {
-	  arrayNew.push(appendString + index);
+    arrayNew.push(appendString + index);
   }
 
   return arrayNew;
