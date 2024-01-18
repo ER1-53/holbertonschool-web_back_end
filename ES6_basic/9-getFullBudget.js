43e9e8ec744f08c9652559e725c0a49eb04bf124
ER1-53 <cremey.erwan@gmail.com>
2024-01-18 12:12:23 +0100

    task 9 correction do function

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,15 +1,11 @@
 import getBudgetObject from './7-getBudgetObject';
 
-export default (income, gdp, capita) => {
+export default function(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
