4176309eb2afad924846722ca65d1812dcca4cf1
ER1-53 <cremey.erwan@gmail.com>
2024-01-18 09:43:05 +0100

    update task 7 extend the array , 10 change code with loop

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,1 +1,9 @@
-export default (array, appendString) => array.map((value) => appendString + value);
+// export default (array, appendString) => array.map((value) => appendString + value);
+export default function appendToEachArrayValue(array, appendString) {
+	const arrayNew = [];
+	for (const index of array) {
+	  arrayNew.push(appendString + index);
+	}
+
+	return arrayNew;
+  }
