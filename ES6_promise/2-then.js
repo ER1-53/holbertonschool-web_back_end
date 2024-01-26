3783764882bb782a7bc8616b8c62a8ee16cb5815
ER1-53 <cremey.erwan@gmail.com>
2024-01-26 20:08:29 +0100

    try with const

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-	  .then(() => ({ status: 200, body: 'success' }))
+	  .then(() => {
+      const response = { status: 200, body: 'success' };
+      return (response);
+    })
 	  .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
