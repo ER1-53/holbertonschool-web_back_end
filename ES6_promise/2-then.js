8adab1351b26a061c6a7a084d60b9e5029771a35
ER1-53 <cremey.erwan@gmail.com>
2024-01-26 20:04:17 +0100

    try return

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-	  .then(() => ({ status: 200, body: 'success' }))
-	  .catch(() => Error())
+	  .then(() => {return ({ status: 200, body: 'success' })})
+	  .catch(() => {return new Error()})
     .finally(() => {
       console.log('Got a response from the API');
     });
