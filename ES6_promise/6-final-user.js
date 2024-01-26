a833a53909a6cecc2c491624167964f15dc08c8e
ER1-53 <cremey.erwan@gmail.com>
2024-01-26 20:45:14 +0100

    change

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,9 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const results = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  return results.map((result) => ({
+  const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return result.map((result) => ({
     status: result.status,
-    value: result.status == 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
+    value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
   }));
 }
