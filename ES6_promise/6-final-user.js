7c7297b5be99398e411aa7ceb9209c853729fd46
ER1-53 <cremey.erwan@gmail.com>
2024-01-26 20:36:02 +0100

    change format & fileName

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status == 'pending' ? result.value : result.reason,
+      value: result.status == 'pending' ? result.value : `Error: ${result.reason}`,
     })));
 }
