c623ee7dc85d826a13764651cb4d04daf4e4c79b
ER1-53 <cremey.erwan@gmail.com>
2024-01-26 09:18:07 +0100

    update task

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise(((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'success' });
     } else {
-      reject(Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
-  }));
+  });
 }
--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   return promise
 	  .then(() => {
-      console.log('Got a response from the API');
       return { status: 200, body: 'success' };
 	  })
 	  .catch(() => {
-      console.log('Got a response from the API');
-      return Error;
-	  });
+      return Error();
+	  })
+    .finally(() => {console.log('Got a response from the API');
+    })
 }
