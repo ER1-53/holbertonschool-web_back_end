8b074209752e91b6200fcdb69dc9b0141267a877
ER1-53 <cremey.erwan@gmail.com>
2024-01-26 20:41:40 +0100

    thx gpt

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
   return results.map((result) => ({
     status: result.status,
     value: result.status == 'fulfilled' ? result.value : `Error: ${result.reason}`,
   }));
 }
+
