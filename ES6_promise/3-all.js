077a92b6f6adf4114bd03457ab40c1a6512733e2
ER1-53 <cremey.erwan@gmail.com>
2024-01-26 20:13:35 +0100

    change array used

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,10 +2,11 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([uploadPhoto, createUser]) => {
-      console.log(uploadPhoto.body, createUser.firstName, createUser.lastName);
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
     });
 }
+
