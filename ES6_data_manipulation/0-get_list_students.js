fd047f06ed6c2ff2ab6793ab5f41bee2ad79ce02
ER1-53 <cremey.erwan@gmail.com>
2024-01-29 11:25:49 +0100

    fix eslint

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
 function getListStudents() {
-	const keys =['id', 'firstName', 'location'];
-	const values = [1, 'Guillaume', 'San Francisco'];
-	const values1 = [2, 'James', 'Columbia'];
-	const values2 = [3, 'Serena', 'San Francisco'];
-	const obj = {};
-	const obj1 = {};
-	const obj2 = {};
-	keys.forEach((key, index) =>{
-		obj[key]= values[index];
-		obj1[key]= values1[index];
-		obj2[key]= values2[index];
-	})
-	const array = []
-	array.push(obj, obj1, obj2);
-	return array;
-};
+  const keys = ['id', 'firstName', 'location'];
+  const values = [1, 'Guillaume', 'San Francisco'];
+  const values1 = [2, 'James', 'Columbia'];
+  const values2 = [3, 'Serena', 'San Francisco'];
+  const obj = {};
+  const obj1 = {};
+  const obj2 = {};
+  keys.forEach((key, index) => {
+    obj[key] = values[index];
+    obj1[key] = values1[index];
+    obj2[key] = values2[index];
+  });
+  const array = [];
+  array.push(obj, obj1, obj2);
+  return array;
+}
 
 export default getListStudents;
